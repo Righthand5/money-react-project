ea8939e6363a4fa45fe300aff54040bdbdc0466a
Righthand5 <xyx1530966653@163.com>
2021-02-18 21:48:43 +0800

    导航栏布局

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,92 @@
 import React from 'react';
 import styled from 'styled-components';
-const Button = styled.button`
-  color:red;
-`;
+import 'index.scss'
+import {
+    HashRouter as Router,
+    Switch,
+    Route,
+    Link,
+    Redirect
+} from "react-router-dom";
+
+
+const Wrapper = styled.div`
+  border: 1px solid red;
+  min-height: 100vh;
+  display:flex;
+  flex-direction: column;
+`
+const Main = styled.div`
+border: 1px solid green;
+flex-grow: 1;
+overflow: auto;
+`
+const Nav = styled.nav`
+  border: 1px solid blue;
+  >ul{
+    display: flex;
+    >li{
+      width: 33.3333%;
+      text-align: center;
+      padding: 16px;
+    }
+  }  
+  
+`
 
 function App() {
-  return (
-    <div>
-      <Button>hello</Button>
-    </div>
-  );
+    return(
+        <Router>
+            <Wrapper>
+                <Main>
+                    <Switch>
+                    <Redirect exact from="/" to = "/money"/>
+                    <Route path="/tags">
+                        <Tags />
+                    </Route>
+                    <Route path="/money">
+                        <Money />
+                    </Route>
+                    <Route path="/Statistics">
+                        <Statistics />
+                    </Route>
+                    <Route path="*">
+                        <NoMatch/>
+                    </Route>
+                </Switch>
+                </Main>
+                <Nav>
+                <ul>
+                    <li>
+                        <Link to="/tags">标签页面</Link>
+                    </li>
+                    <li>
+                        <Link to="/money">记账页面</Link>
+                    </li>
+                    <li>
+                        <Link to="/Statistics">统计页面</Link>
+                    </li>
+                </ul>
+            </Nav>
+        </Wrapper>
+        </Router>
+    );
+}
+function NoMatch(){
+    return(
+        <div>404 error</div>
+    )
+}
+function Tags() {
+    return <h2>标签页面</h2>;
+}
+
+function Money() {
+    return <h2>记账页面</h2>;
+}
+
+function Statistics() {
+    return <h2>统计页面</h2>;
 }
 
 export default App;
