acea14c9932885c648b1392a0b80029594d8c81e
Righthand5 <xyx1530966653@163.com>
2021-02-23 22:20:46 +0800

    完成标签的选择和新增

---
+++ src/views/Money/MyLayout.tsx
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+import Layout from '../../components/Layout';
+
+const MyLayout = styled(Layout)`{
+  display: flex;
+  flex-direction: column;
+}`
+export {MyLayout}
