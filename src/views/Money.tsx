dfeec7f0f5293794b9d5d809071960ce6fca8773
Righthand5 <xyx1530966653@163.com>
2021-02-25 15:35:35 +0800

    numberPad 功能 wip(working in progress)

--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -1,44 +1,16 @@
 import React from 'react';
 import {CategorySection} from './Money/CategorySection';
 import {TagSection} from './Money/TagSection';
-import {NoteSection} from './Money/NoteSection';
+import {NoteSection} from "./Money/NoteSection";
 import {NumberPadSection} from './Money/NumberPadSection';
 import {MyLayout} from './Money/MyLayout';
 function Money() {
     return (
         <MyLayout>
-            <TagSection>
-                <ol>
-                    <li>衣</li>
-                    <li>食</li>
-                    <li>住</li>
-                    <li>行</li>
-                </ol>
-                <button>新增标签</button>
-            </TagSection>
+            <TagSection/>
             <NoteSection/>
             <CategorySection/>
-            <NumberPadSection>
-                <div className="output">
-                    100
-                </div>
-                <div className="pad clearfix">
-                    <button>1</button>
-                    <button>2</button>
-                    <button>3</button>
-                    <button>删除</button>
-                    <button>4</button>
-                    <button>5</button>
-                    <button>6</button>
-                    <button>清空</button>
-                    <button>7</button>
-                    <button>8</button>
-                    <button>9</button>
-                    <button className="ok">OK</button>
-                    <button className="zero">0</button>
-                    <button className="dot">.</button>
-                </div>
-            </NumberPadSection>
+            <NumberPadSection/>
         </MyLayout>
     );
 }
