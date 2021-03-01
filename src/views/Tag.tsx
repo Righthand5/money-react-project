3c576e70818f7aa89b95f95bedb42e96cce278a7
Righthand5 <xyx1530966653@163.com>
2021-03-01 18:38:01 +0800

    Button样式

--- src/views/Tag.tsx
+++ src/views/Tag.tsx
@@ -2,16 +2,34 @@ import React from 'react';
 import {useTags} from '../useTags'
 import {useParams} from "react-router-dom"
 import Layout from '../components/Layout';
+import Icon from '../components/icon';
+import {Button} from '../components/Button';
+
 type Params = {
     id:string;
 }
+
+
 const Tag: React.FC = (props)=>{
     const {findTag} = useTags()
     let { id } = useParams<Params>()
     const tag = findTag(parseInt(id))
     return(
         <Layout>
-            <div>{tag.name}</div>
+            <header>
+                <Icon name = "left"/>
+                <span>编辑标签</span>
+            </header>
+            <div>
+                <label>
+                    <span>标签名</span>
+                    <input type="text" placeholder="在这里添加备注"/>
+                </label>
+            </div>
+            <Button>删除标签</Button>
+            <div>
+
+            </div>
         </Layout>
     )
 }
