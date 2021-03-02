5379479667118d80199de16fa3cb50d7a4da07a2
Righthand5 <xyx1530966653@163.com>
2021-03-02 14:50:23 +0800

    history.goBack()

--- src/useTags.tsx
+++ src/useTags.tsx
@@ -19,19 +19,11 @@ const useTags = () =>{
         }
         return result;
     }
-    const updateTag = (id:number,obj:{name:string}) =>{
-        const index = findTagIndex(id);
-        //深拷贝 tags 得到 tagsClone
-        const tagsClone = JSON.parse(JSON.stringify(tags));
-        //把 tagsClone 的第index删掉，换成{id：id，name:obj.name}
-        tagsClone.splice(index,1,{id:id,name:obj.name});
-        setTags(tagsClone)
+    const updateTag = (id:number,name:{name:string}) =>{
+        setTags(tags.map(tag => tag.id === id ? {id,name:name.name}:tag))
     }
     const  deleteTag = (id: number) =>{
-        const index = findTagIndex(id);
-        const tagsClone = JSON.parse(JSON.stringify(tags))
-        tagsClone.splice(index,1)
-        setTags(tagsClone)
+        setTags(tags.filter(tag => tag.id !== id))
     }
     return {tags,setTags,findTag,updateTag,findTagIndex,deleteTag}
 };
